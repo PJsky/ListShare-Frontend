8f7ebfb15d204055af532dcf739ed388c2f4b4c1
PiotrJ <jakubowskypiotr@gmail.com>
2020-07-29 17:40:41 +0200

    minimal mainpage setup

--- listshare-front/src/components/MainPage.js
+++ listshare-front/src/components/MainPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
 const Nav = () => {
@@ -6,7 +7,28 @@ const Nav = () => {
     return(
         <>
             <div className="mainpage-container">
-                <h1>Hello World!</h1>
+                {/* <h1>Hello!</h1> */}
+                <h2>Search for a list</h2>
+                <Formik
+                initialValues={{ searchList:'' }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        console.log("searching for a list")
+                    }, 400);
+                }}
+                >
+                {({ isSubmitting }) => (
+                    <Form>
+                    <Field type="text" name="searchList" className=""/>
+                    <ErrorMessage name="searchList" component="div" />
+                    <button type="submit" disabled={isSubmitting} className="">
+                        Search
+                    </button>
+                    </Form>
+                )}
+                </Formik>
+                <p>or</p>
+                <button className="create-list-button">Create List</button>
             </div>
         </>
     )
