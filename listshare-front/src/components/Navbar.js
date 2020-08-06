f402127456473ed9abcb7cc018461be8105c5789
PiotrJ <jakubowskypiotr@gmail.com>
2020-08-06 15:32:37 +0200

    Add some resposivness

--- listshare-front/src/components/Navbar.js
+++ listshare-front/src/components/Navbar.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { user_log_out } from '../actions/userActions';
+import { FaBars } from 'react-icons/fa';
 
 
 const Nav = () => {
     const dispatch = useDispatch();
     const history = useHistory();
     const logged = useSelector(state => state.logged_in);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     
     const getNav = () =>{
         if(!logged)return(
+            <>
             <ul className="navigation-list">
                 <li className="navigation-list-item">
                     <Link to ="/login">Login</Link>
@@ -19,16 +22,45 @@ const Nav = () => {
                     <Link to ="/register">Register</Link>
                 </li>
             </ul>
+            <ul className="navigation-list-dropdown">
+                <li className="navigation-list-dropdown-trigger"
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                    <FaBars/>
+                </li>
+            </ul>
+            </>
         );
         else
         return(
+            <>
             <ul className="navigation-list">
                 <li className="navigation-list-item">
                     <Link to ="./login"
                     onClick={logOut}>Log Out</Link>
                 </li>
             </ul>
+            <ul className="navigation-list-dropdown">
+                <li className="navigation-list-dropdown-trigger"
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                    <FaBars/>
+                </li>
+            </ul>
+            </>
         );
+    }
+
+    const getDropdownNav = () => {
+        if(!logged) return(
+            <>
+            <Link to ="/login"><div className="navigation-dropdown-item">Login</div></Link>
+            <Link to ="/register"><div className="navigation-dropdown-item">Register</div></Link>
+            </>
+        )
+        else if(logged) return(
+            <>
+                <Link to ="/login" onClick={logOut}><div className="navigation-dropdown-item">Log Out</div></Link>
+            </>
+        )
     }
 
     const logOut = () =>{
@@ -50,6 +82,9 @@ const Nav = () => {
                 </div>
                 <nav className="navigation">
                     {getNav()}
+                </nav>
+                <nav className={isDropdownOpen?"navigation-dropdown":"hidden-class"}>
+                    {getDropdownNav()}
                 </nav>
             </div>
         </>
